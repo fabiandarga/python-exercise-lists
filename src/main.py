c0672eea1e25756c7b5f1c85cdf29c8e0e5eace0
Fabian Darga <kontakt@fabiandarga.de>
2024-10-21 06:38:45 +0200

    Add exercise code

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
-def add_numbers(a, b):
-    return a + b
+def gerade_zahlen():
+    # Implementierung hier
+    return
+
+
+def quadrierte_zahlen(zahlen):
+    # Implementierung hier
+    return
+
+
+def gefilterte_woerter(woerter):
+    # Implementierung hier
+    return
+
+
+def flache_liste(verschachtelte_liste):
+    # Implementierung hier
+    return
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,31 @@
 import unittest
-import src.main as main
+from src.main import (
+    gerade_zahlen,
+    quadrierte_zahlen,
+    gefilterte_woerter,
+    flache_liste
+)
 
+class TestListComprehension(unittest.TestCase):
 
-class TestMain(unittest.TestCase):
-    def test_main(self):
-        self.assertIsNotNone(main.test_1, msg="Die Funktion xxx gibt es noch nicht.")
+    def test_gerade_zahlen(self):
+        erwartete_ausgabe = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+        self.assertEqual(erwartete_ausgabe, gerade_zahlen())
 
-    def test_add_numbers_pass(self):
-        self.assertEqual(main.add_numbers(2, 3), 5)
+    def test_quadrierte_zahlen(self):
+        zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        erwartete_ausgabe = [25, 36, 49, 64, 81, 100]
+        self.assertEqual(erwartete_ausgabe, quadrierte_zahlen(zahlen))
 
+    def test_gefilterte_woerter(self):
+        woerter = ['apfel', 'banane', 'kirsche', 'dattel', 'erdbeere', 'feige', 'traube']
+        erwartete_ausgabe = ['erdbeere']
+        self.assertEqual(erwartete_ausgabe, gefilterte_woerter(woerter))
+
+    def test_flache_liste(self):
+        verschachtelte_liste = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        erwartete_ausgabe = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(erwartete_ausgabe, flache_liste(verschachtelte_liste))
 
 if __name__ == '__main__':
     unittest.main()
